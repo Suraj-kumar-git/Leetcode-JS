e77b744eb29a101e70cced2f6eb3730fd4d4d978
Suraj Kumar <navneetraj07903@gmail.com>
2023-06-11 09:45:24 +0530

    2nd Day Basic Array Transformations

---
+++ 30 Days of JS/2626. Array Reduce Transformation/Solution.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    if(nums.length===0){
+        return init;
+    }
+    var val=init;
+    for(let i=0;i<nums.length;i++){
+        val=fn(init,nums[i]);
+        init=val;
+    }
+    
+    return val;
+};
